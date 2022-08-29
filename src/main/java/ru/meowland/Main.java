76e7d8d6d27bb5bcc5801de6c137314339d0bac7
KotMilkMeoWtwo <forgithubkotmilkmeow@gmail.com>
2022-08-29 14:01:01 +0300

    meow

--- src/main/java/ru/meowland/Main.java
+++ src/main/java/ru/meowland/Main.java
@@ -1,5 +1,6 @@
 package ru.meowland;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -13,7 +14,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.rmi.MarshalException;
+import java.io.File;
 import java.util.Objects;
 
 public final class Main extends JavaPlugin implements Listener {
@@ -22,6 +23,8 @@ public final class Main extends JavaPlugin implements Listener {
     public void onEnable() {
         getLogger().info("meow");
         getServer().getPluginManager().registerEvents(this, this);
+        getConfig().options().copyDefaults(true);
+        reloadConfig();
     }
 
     @Override
@@ -34,7 +37,7 @@ public final class Main extends JavaPlugin implements Listener {
         Player killer = p.getKiller();
 
         if(killer != null){
-            ItemStack itemStack = new ItemStack(setName( new ItemStack(Material.DIAMOND, 1, (short) 0), "Lucky block &l&6lvl 5"));
+            ItemStack itemStack = new ItemStack(setName( new ItemStack(Material.DIAMOND, 1, (short) 0), "Lucky block lvl 5"));
             killer.getInventory().addItem(itemStack);
             killer.sendMessage("u get diamond");
         }
@@ -49,7 +52,7 @@ public final class Main extends JavaPlugin implements Listener {
                 for(int i = 0; i < p.getInventory().getSize(); i++){
                     if(p.getInventory().getItem(i) != null){
                         if(Objects.requireNonNull(p.getInventory().getItem(i)).getType() == Material.DIAMOND){
-                            ItemStack d = new ItemStack(Material.DIAMOND, 1);
+                            ItemStack d = new ItemStack(setName(new ItemStack(Material.DIAMOND, 1), "Lucky block lvl 5"));
                             p.getInventory().removeItem(d);
                             break;
                         }
@@ -57,18 +60,23 @@ public final class Main extends JavaPlugin implements Listener {
 
                 }
                 //p.getInventory().removeItem(p.getInventory().getItem());
-                int rand = (int) (Math.random()*2);
+                int rand = (int) (Math.random()*3);
                 ItemStack p0 = new ItemStack(Material.DIAMOND_SWORD, 1);
                 p0.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 3);
                 p0.addUnsafeEnchantment(Enchantment.FIRE_ASPECT, 2);
                 ItemStack p1 = new ItemStack(Material.DIAMOND_BOOTS, 1);
                 p1.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1);
+                ItemStack p2 = new ItemStack(Material.DIAMOND_HELMET);
                 switch (rand){
                     case (0):
                         p.getInventory().addItem(p0);
                         break;
                     case (1):
                         p.getInventory().addItem(p1);
+                        break;
+                    case (2):
+                        p.getInventory().addItem(p2);
+                        break;
                 }
             }
         }
